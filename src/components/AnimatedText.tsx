24c13123667f94b0e94385dbfbe0d88c97cd47ff
madushan <ranasinhemadushan@gmail.com>
2023-04-02 21:55:40 +0530

    AnimatedText Component Created

---
+++ src/components/AnimatedText.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+
+interface AnimatedTextProps {
+  text?: string;
+  className?: string;
+}
+
+const Layout: React.FC<AnimatedTextProps> = ({ text, className }) => {
+  return (
+    <div
+      className={`w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden  `}
+    >
+      <h1
+        className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
+      >
+        {text?.split(" ").map((word, index) => {
+          return (
+            <span key={word + "-" + index} className="inline-block">
+              {word}&nbsp;
+            </span>
+          );
+        })}
+      </h1>
+    </div>
+  );
+};
+
+export default Layout;
