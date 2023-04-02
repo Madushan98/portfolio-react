0349e96019517dc47925662d3fbe56437db03939
madushan <ranasinhemadushan@gmail.com>
2023-04-02 23:06:30 +0530

    external files added

---
+++ src/common/data.tsx
@@ -0,0 +1,38 @@
+import { Project } from "./types";
+
+const projects: Project[] = [
+  {
+    id: 1,
+    title: "Deep Learning-based Virtual Assistant in Sinhala Language II  ",
+    description:
+      "Deep Learning, Tensorflow, Python, Fast Api, Next.Js, Docker This project implements a versatile and responsive deep learning-based virtual assistant in the Sinhala language, using a hierarchical transformer model.",
+    imageUrl: "https://example.com/project1.jpg",
+    linkUrl: "https://example.com/project1",
+  },
+  {
+    id: 2,
+    title: "TrackMate - Digital and Authentic Lock-down Pass Generator",
+    description:
+      "The project is implemented for use in lock-down situations to generate and authenticate passes in lock-down areas and also provide further information about spreading by tracking infected. The project  uses microservice architecture with Asp.net web API for back-end and  Angular for the front-end, Flutter for front-end and mobile implementation. ",
+    imageUrl: "https://example.com/project2.jpg",
+    linkUrl: "https://example.com/project2",
+  },
+  {
+    id: 3,
+    title: "Moviero (Movie Streaming Platform)",
+    description:
+      "This project is a user-friendly movie streaming full stack web application for streaming video content. The project is developed using Spring boot, React, MySQL, and Cloudinary technologies",
+    imageUrl: "https://example.com/project2.jpg",
+    linkUrl: "https://example.com/project2",
+  },
+  {
+    id: 4,
+    title: "Garage Lanka Mobile App",
+    description:
+      "Due to the current economic crisis in Sri Lanka, citizens are in great trouble finding required fuel types and LP gas for their day today work. This app allows LP gas dealers and fuel stations to update fuel availability based on different fuel types, whereas users are able to find the nearest places to buy necessary fuel and gas.",
+    imageUrl: "https://example.com/project2.jpg",
+    linkUrl: "https://example.com/project2",
+  },
+];
+
+export default projects;
