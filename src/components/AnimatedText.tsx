1cf72d2da3e91de7501420c4ec1a517aefe94f34
madushan <ranasinhemadushan@gmail.com>
2023-04-02 23:05:58 +0530

    Animated Text Updated

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -8,7 +8,7 @@ interface AnimatedTextProps {
 const Layout: React.FC<AnimatedTextProps> = ({ text, className }) => {
   return (
     <div
-      className={`w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden  `}
+      className={`w-full mx-auto pt-1 flex items-center justify-center text-center overflow-hidden  `}
     >
       <h1
         className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className}`}
