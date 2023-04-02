ae2ff5fa65cf3379c7bd55c1a07412756344d97f
madushan <ranasinhemadushan@gmail.com>
2023-04-02 23:05:06 +0530

    Project Page Created

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout";
+import { Project } from "../common/types";
+import projects from "../common/data";
+import ProjectCard from "../components/ProjectCard";
+import ScrollableComponent from "../components/Scrollable";
+
+const Projects: React.FC = () => {
+  const projectList: Project[] = projects;
+  return (
+    <div>
+      <main className="flex items-center text-dark w-full min-h-[100%] ">
+        <Layout className="pt-10">
+          <ScrollableComponent children={projectList}></ScrollableComponent>
+        </Layout>
+      </main>
+    </div>
+  );
+};
+
+export default Projects;
